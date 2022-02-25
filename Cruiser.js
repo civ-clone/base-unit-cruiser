99cd17833ea6cfd7f2c87529cc54f01d2105172d
dom111 <dom111@users.noreply.github.com>
2022-02-25 21:14:12 +0000

    Initial commit.

---
+++ Cruiser.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Cruiser = void 0;
+const Fortifiable_1 = require("@civ-clone/base-unit-type-fortifiable/Fortifiable");
+class Cruiser extends Fortifiable_1.Fortifiable {
+}
+exports.Cruiser = Cruiser;
+exports.default = Cruiser;
+//# sourceMappingURL=Cruiser.js.map
---
+++ README.md
@@ -0,0 +1,4 @@
+# base-unit-cruiser
+
+This is a template repo.
+
